f12e55dec33a3ab111bc67931f522f4144418ad3
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-01-09 15:07:21 -0800

    users controller create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(user_path(@user))
+    else
+      redirecto_to(new_user_path)
+    end
   end
 
   def edit
