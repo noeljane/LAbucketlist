8d2f28d0ad91d0663220657231e91642aafdec3e
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-01-13 23:22:17 -0800

    experimenting with bootstrap and password authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  
   root 'activities#index'
+  
   resources :sessions, only: [:new, :create]
   resources :users 
   
@@ -10,6 +12,10 @@ Rails.application.routes.draw do
   post '/activities/:id/copy' => 'activities#copy', as: :copy_activity
 
   delete '/logout' => 'sessions#destroy', as: :logout
+
+  resources :account_activations, only: [:edit]
+
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
