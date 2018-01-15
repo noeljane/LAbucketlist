0511dfb057d197b68a578f335ec79b58f8feca4d
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-01-15 10:47:32 -0800

    Fixed flash message bug with new activity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authorize, only: [:show, :create]
+  before_action :authorize, only: [:create]
   
   def index
     @users = User.all
